8db515f319b70a4830731fd77e9096ad9ddb5c70
amiya-perceptive <amiya@perceptive-solutions.com>
2021-09-07 20:15:23 +0530

    complte user schema model

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,40 +1,75 @@
-var mongooes = require("mongoose");
-var userSchema = new mongoose.Schema(
-    {
-        name:{
-           type:String,
-           required:true,
-           length:32,
-           trim:true 
-        },
-        lastname:{
-            type:String,
-            length:32,
-            trim:true
-        },
-        email:{
-            type:String,
-            trim:true,
-            required:true,
-            unique:true
-        },
-        userinfo:{
-            type:String,
-            trim:true
-        },
-        salt:String,
-        role:{
-            type:Number,
-            default:0
-        },
-        purchases:{
-            type:Array,
-            default:[]
-        }
-    },
-    {
-        timestamps:true
-    }
-);
+var mongoose = require("mongoose");
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
 
-module.exports = mongooes.model("User",userSchema);
+var userSchema = new mongoose.Schema(
+{
+    name:{
+       type:String,
+       required:true,
+       length:32,
+       trim:true 
+    },
+    lastname:{
+        type:String,
+        length:32,
+        trim:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true
+    },
+    userinfo:{
+        type:String,
+        trim:true
+    },
+    encry_password:{
+        type:String,
+        required:true,
+    },
+    salt:String,
+    role:{
+        type:Number,
+        default:0
+    },
+    purchases:{
+        type:Array,
+        default:[]
+    }
+},
+{
+    timestamps:true
+});
+
+userSchema.virtual("password")
+          .set(function(password){
+            this._password = password;
+            this.salt = uuidv1();
+            this.encry_password = this.securePassword(password);
+            return this._password;
+          })
+          .get(function(){
+              return this._password;
+          }) 
+
+userSchema.methods = {
+
+     authenticate:function(rawPassword){
+       return this.securePassword(rawPassword.password) === this.encry_password;
+     },
+
+    securePassword:function(rawPassword){
+        if(!rawPassword) return "";
+        try {
+           return crypto.createHmac('sha256', this.salt)
+                   .update(rawPassword)
+                   .digest('hex');
+        } catch (error) {
+            return ""
+        }
+    }
+}
+
+module.exports = mongoose.model("User",userSchema);
