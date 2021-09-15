759d0428ff97cc32cde0bd186c73e59ec7a0ff9d
amiya-perceptive <amiya@perceptive-solutions.com>
2021-09-15 19:06:20 +0530

    complte model creation

---
+++ projbackend/models/product.js
@@ -0,0 +1,48 @@
+var mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema;
+
+var productSchema = new mongoose.Schema(
+    {
+        name:{
+           type:String,
+           trim:true,
+           required:true,
+           maxlength:32,
+           unique:true
+        },
+        description:{
+          type:String,
+          trim:true,
+          required:true,
+          maxlength:2000
+        },
+        price:{
+           type:Number,
+           trim:true,
+           required:true,
+           maxlength:32
+        },
+        category:{
+           type:ObjectId,
+           ref:"Category",
+           required:true
+        },
+        stock:{
+            type:Number
+        },
+        sold:{
+            type:Number,
+            default:0
+        },
+        photo:{
+            type:Buffer,
+            contentType:String
+        }
+    },
+       
+    {
+        timestamps:true
+    }
+);
+
+module.exports = mongoose.model("Product",productSchema);
